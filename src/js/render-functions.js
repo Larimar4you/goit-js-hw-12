c02856ea53c1204a7277f64fef710511796abba5
Larimar4you <Zajanezlaja@gmail.com>
2025-02-24 11:01:53 +0100

    Автокоммит

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,30 +1,21 @@
 export function renderGallery(images) {
   const gallery = document.querySelector('.gallery');
 
+  if (!gallery) return; // Проверяем, что элемент существует
+
   const markup = images
     .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `
-          <div class="gallery_item">
-            <a href="${largeImageURL}" class="gallery_link">
-              <img src="${webformatURL}" alt="${tags}" class="gallery_image" loading="lazy" />
-            </a>
-            <div class="info">
-              <p class="info-item"><b>Likes:</b> ${likes}</p>
-              <p class="info-item"><b>Views:</b> ${views}</p>
-              <p class="info-item"><b>Comments:</b> ${comments}</p>
-              <p class="info-item"><b>Downloads:</b> ${downloads}</p>
-            </div>
+      image => `
+        <a class="gallery_item" href="${image.largeImageURL}">
+          <img class="gallery_image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+            <p class="info-item"><b>Views:</b> ${image.views}</p>
+            <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+            <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
           </div>
-          `
+        </a>
+      `
     )
     .join('');
 
@@ -32,21 +23,34 @@ export function renderGallery(images) {
 }
 
 export function clearGallery() {
-  document.querySelector('.gallery').innerHTML = '';
+  const gallery = document.querySelector('.gallery');
+  if (gallery) gallery.innerHTML = '';
 }
 
 export function showLoadMoreButton() {
-  document.querySelector('.load-more').classList.remove('hidden');
+  const loadMoreBtn = document.querySelector('.load-more');
+  if (loadMoreBtn) {
+    loadMoreBtn.style.display = 'block';
+  }
 }
 
 export function hideLoadMoreButton() {
-  document.querySelector('.load-more').classList.add('hidden');
+  const loadMoreBtn = document.querySelector('.load-more');
+  if (loadMoreBtn) {
+    loadMoreBtn.style.display = 'none';
+  }
 }
 
 export function showLoader() {
-  document.querySelector('.loader').classList.add('visible');
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.classList.add('visible');
+  }
 }
 
 export function hideLoader() {
-  document.querySelector('.loader').classList.remove('visible');
+  const loader = document.querySelector('.loader');
+  if (loader) {
+    loader.classList.remove('visible');
+  }
 }
