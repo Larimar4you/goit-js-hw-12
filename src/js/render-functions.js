af515cb6690d64bd4599e6f1b67915a0886b03ea
Larimar4you <Zajanezlaja@gmail.com>
2025-03-01 15:52:10 +0100

    Автокоммит

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -71,4 +71,5 @@ export function hideLoader() {
 export function toggleLoadMoreButton(isVisible) {
   const loadMoreBtn = document.querySelector('.load-more');
   if (!loadMoreBtn) return;
+  loadMoreBtn.style.display = isVisible ? 'block' : 'none';
 }
